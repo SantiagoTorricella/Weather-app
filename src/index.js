48fcfa250f57e472f6f85dd11866b98c56a59e96
Santiago Torricella <torrisantiago@gmail.com>
2023-08-07 12:09:51 -0300

    Last tweaks and responsive ui

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ const localTime = document.querySelector(".local-time");
 // ==== VARIABLES ====
 let currentCity = "";
 let currentClimate = "";
-let datos = "";
 
 // ==== SEARCH FOR THE WEATHER OF THE DESIRED CITYD ====
 async function getAndSaveData() {
@@ -27,9 +26,16 @@ async function getAndSaveData() {
   console.log(currentClimate);
 }
 
-searchButton.addEventListener("click", () => {
-  pushData();
-});
+// ==== FORMAT DATE ====
+function getDayFormated() {
+  let date = currentClimate.location.localtime.split(" ")[0];
+  let day = date.split("-")[2];
+  let month = date.split("-")[1];
+  let year = date.split("-")[0];
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  let d = new Date(year, month, day);
+  return days[d.getDay()] + " | " + day + "/" + month + "/" + year;
+}
 
 // ==== ASYNC FUNCTION TO PUSH THE DATA ON SCREEN ====
 async function pushData() {
@@ -38,9 +44,20 @@ async function pushData() {
   cityProv.innerText = currentClimate.location.region;
   cityCountry.innerText = currentClimate.location.country;
   climateImg.src = currentClimate.current.condition.icon;
-  temperature.innerText = currentClimate.current.temp_c + "°C";
+  temperature.innerText =
+    currentClimate.current.temp_c +
+    "°C" +
+    " " +
+    "/" +
+    " " +
+    currentClimate.current.temp_f +
+    "°F";
   condition.innerText = currentClimate.current.condition.text;
-  localTime.innerText = currentClimate.location.localtime;
+  localTime.innerText = getDayFormated();
 }
 
 pushData();
+
+searchButton.addEventListener("click", () => {
+  pushData();
+});
